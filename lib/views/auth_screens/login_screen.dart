152f6afd479491ccdcf91ac19b35f535cd2ecbc7
Lubna-Mehboob <lubnamehboob7@gmail.com>
2024-11-14 22:56:35 +0500

    update

--- lib/views/auth_screens/login_screen.dart
+++ lib/views/auth_screens/login_screen.dart
@@ -8,6 +8,7 @@ import 'package:todoapp/constants/app_assets/images.dart';
 import 'package:todoapp/constants/colors/app_colors.dart';
 import 'package:todoapp/controllers/components/account_exists_component.dart';
 import 'package:todoapp/controllers/components/apploader/apploader.dart';
+import 'package:todoapp/controllers/components/conatiner_component.dart';
 import 'package:todoapp/controllers/components/custom_button_component.dart';
 import 'package:todoapp/controllers/components/image_component.dart';
 import 'package:todoapp/controllers/components/primary_text_component.dart';
@@ -72,56 +73,65 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const PrimaryTxtComponent(
-              appText: 'WelCome Back!',
-              appTextSize: 20,
-              appTextWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          const Positioned(
+            top: -5,
+            left: -15,
+            child: ConatinerComponent(),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const PrimaryTxtComponent(
+                  appText: 'WelCome Back!',
+                  appTextSize: 20,
+                  appTextWeight: FontWeight.w500,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ImageComponent(appImage: AppImages.loginImage),
+                const SizedBox(
+                  height: 20,
+                ),
+                TxtFormFieldComponent(
+                    textHint: 'Enter Email',
+                    appController: _emailController,
+                    appIcons: Icons.email_outlined),
+                TxtFormFieldComponent(
+                  textHint: 'Enter Password',
+                  appController: _passowrdController,
+                  appIcons: Icons.lock,
+                  isTextObscure: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                isLoading
+                    ? const AppLoader()
+                    : ButtonComponent(
+                        buttonText: 'Login',
+                        onbuttonTap: () {
+                          accountLogin();
+                          // Navigator.push(
+                          //   context,
+                          //   CupertinoPageRoute(
+                          //     builder: (context) => const HomeScreen(),
+                          //   ),
+                          // );
+                          // Get.to(() => const HomeScreen());
+                        }),
+                const AccountExists(
+                  rowText: 'Don\'t have an account?',
+                  nextScreen: RegistrationScreen(),
+                  rowText2: 'Register',
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            ImageComponent(appImage: AppImages.loginImage),
-            const SizedBox(
-              height: 20,
-            ),
-            TxtFormFieldComponent(
-                textHint: 'Enter Email',
-                appController: _emailController,
-                appIcons: Icons.email_outlined),
-            TxtFormFieldComponent(
-              textHint: 'Enter Password',
-              appController: _passowrdController,
-              appIcons: Icons.lock,
-              isTextObscure: true,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            isLoading
-                ? const AppLoader()
-                : ButtonComponent(
-                    buttonText: 'Login',
-                    onbuttonTap: () {
-                      accountLogin();
-                      // Navigator.push(
-                      //   context,
-                      //   CupertinoPageRoute(
-                      //     builder: (context) => const HomeScreen(),
-                      //   ),
-                      // );
-                      // Get.to(() => const HomeScreen());
-                    }),
-            const AccountExists(
-              rowText: 'Don\'t have an account?',
-              nextScreen: RegistrationScreen(),
-              rowText2: 'Register',
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
